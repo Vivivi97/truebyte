935797a0a03c88001bd85ab4e3a708146993df49
David Rodriguez <David07.97r@gmail.com>
2026-07-02 14:13:34 -0600

    "Fix: Agregados PropTypes, eliminado fragmento redundante y mejorado alt de imagen"

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 
 function Card({ title, imgsrc, parrafo }) {
   return (
-    <>
       <div className="col-md-4 col-10 mx-auto">
         <div className="card bg-white">
-          <img src={imgsrc} className="card-img-top bg-light" alt="..." />
+          <img src={imgsrc} className="card-img-top bg-light" alt={title} />
           <div className="card-body text-white bg-dark">
             <h5 className="card-title bg-dark">{title}</h5>
             <p className="card-text bg-dark">{parrafo}</p>
@@ -16,8 +16,15 @@ function Card({ title, imgsrc, parrafo }) {
           </div>
         </div>
       </div>
-    </>
   );
 }
 
+// PropTypes para validación de props
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  imgsrc: PropTypes.string.isRequired,
+  parrafo: PropTypes.string.isRequired,
+};
+
 export default Card;
+export { Card };
